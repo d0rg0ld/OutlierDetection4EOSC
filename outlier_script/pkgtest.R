06e96a531364ab51ae3b631d9debde7b68e96b8a
Doron Goldfarb <doron.goldfarb@gmail.com>
2020-05-18 14:52:25 +0200

    Update pkgtest.R

--- outlier_script/pkgtest.R
+++ outlier_script/pkgtest.R
@@ -1,4 +1,10 @@
-#This work is co-funded by the EOSC-hub project (Horizon 2020) under Grant number 777536.  
+
+#This work is co-funded by the EOSC-hub project (Horizon 2020) under Grant number 777536. 
+
+# Doron Goldfarb (doron DOT goldfarb AT umweltbundesamt DOT at)
+# Johannes Kobler (johannes DOT kobler AT umweltbundesamt DOT at)
+
+# Environment Agency Austria, 2020
 
 pkgTest <- function(x, mode="regular", extra="")
 {
