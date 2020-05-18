1414542a7269389dac1b65de85e821b0753eb162
Doron Goldfarb <doron.goldfarb@gmail.com>
2020-05-18 14:52:40 +0200

    Update quality_check.R

--- outlier_script/quality_check.R
+++ outlier_script/quality_check.R
@@ -1,4 +1,10 @@
-#This work is co-funded by the EOSC-hub project (Horizon 2020) under Grant number 777536.
+
+#This work is co-funded by the EOSC-hub project (Horizon 2020) under Grant number 777536. 
+
+# Doron Goldfarb (doron DOT goldfarb AT umweltbundesamt DOT at)
+# Johannes Kobler (johannes DOT kobler AT umweltbundesamt DOT at)
+
+# Environment Agency Austria, 2020
 
 options(encoding = "UTF-8")
 
